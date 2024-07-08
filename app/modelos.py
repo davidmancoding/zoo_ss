61d6aa035a74ed22fe00cbaf6f0bf02b4942e004
David Nieto <davidnieto2211@gmail.com>
2024-07-08 17:26:40 -0400

    V Agregando logica de varias entradas

--- app/modelos.py
+++ app/modelos.py
@@ -8,6 +8,7 @@ class TipoEntrada(Enum):
 
 
 class Entrada:
+
     def __init__(self, edad: int):
         if edad <= 2:
           self.tipo = TipoEntrada.BEBE
@@ -21,3 +22,25 @@ class Entrada:
         else:                      
           self.tipo = TipoEntrada.JUBILADO
           self.precio = 18
+
+class Grupo_Entrada:
+   
+    def __init__(self):
+        self.total = 0
+        self.num_entradas = 0
+
+    def add_entrada(self, edad):
+        nueva_entrada = Entrada(edad)
+        self.num_entradas += 1
+        self.total += nueva_entrada.precio
+
+    def add_entradas(self, edades):
+        for edad in edades:
+            self.add_entrada(edad)
+
+'''
+      en funcion de la edad crear una entrada, incrementar el contador de entrada.
+      Con el precio de la entrada nueva incrementar el total
+      Tiene que pasar el test
+
+'''
